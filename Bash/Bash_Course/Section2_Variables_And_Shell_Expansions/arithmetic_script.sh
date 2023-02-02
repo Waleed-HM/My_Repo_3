dc21689e4b95d6d9eaf1c2b2c2dedbcbc3d57373
Waleed-HM <waleed.alhammami@spaceconcordia.ca>
2023-02-02 11:21:31 -0500

    Dealing with decimal numbers

--- Bash/Bash_Course/Section2_Variables_And_Shell_Expansions/arithmetic_script.sh
+++ Bash/Bash_Course/Section2_Variables_And_Shell_Expansions/arithmetic_script.sh
@@ -9,3 +9,11 @@ echo $((x / y))
 echo $((2 + 3 * 4))
 echo $((4 ** 2))		# exponentiation
 echo $((4 % 2))			# remainder
+
+# Dealing with decimal numbers
+# The "bc" command is used in Linux to deal with decimal point numbers, among other things
+# we can do things like fractional division by using it with scale (number of digits after the decimal point) command and piping to bc
+echo "scale=2; 5/2" | bc
+
+# Note : exponentiation in the bc command is done like a^b, rather than a**b
+echo "3^3" | bc
