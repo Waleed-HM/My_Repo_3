e5059c9dc8d1bc69ca728ecfce85056b07ee01f2
Waleed <waleed.alhammami@spaceconcordia.ca>
2023-09-18 12:20:40 -0400

    Added first unit test

---
+++ C_Unit_Testing/tests/average/test.cpp
@@ -0,0 +1,40 @@
+#include "CppUtest/TestHarness.h"
+
+
+// This part is because our module to be tested (average.h) is written in C
+// If it was written in C++, then we would just have to include it normally
+extern "C" {
+	#include "average.h"
+}
+
+// Create a test group
+TEST_GROUP(average_test_group)
+{
+	void setup()
+	{
+		// Initialize before each test
+		// If we have anything to perform before each test in this group, we can do it here
+	}
+
+	void teardown()
+	{
+		// Deinitialize after each test
+	}
+}
+
+// Test the average function
+TEST(average_test_group, simple_test)
+{
+	float array[] = {-1.0, 0.0, 1.0, 2.0, 3.0};
+	float avg = average(array, 5);
+	CHECK_EQUAL(avg, 1.0);
+}
+
+// It's generally a good idea to test edge cases
+// Test null array
+TEST(average_test_group, null_test)
+{
+	float array[1];
+	float avg = average(array, 0);
+	CHECK_EQUAL(avg, 0.0);
+}
