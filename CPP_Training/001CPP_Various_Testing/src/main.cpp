5409a4339fc0a7d97bf9759d38ee451667ac2432
Waleed <waleed.alhammami@spaceconcordia.ca>
2023-10-01 10:27:21 -0400

    Small change and update to .gitignore

--- CPP_Training/001CPP_Various_Testing/src/main.cpp
+++ CPP_Training/001CPP_Various_Testing/src/main.cpp
@@ -7,6 +7,10 @@ int main(int argc,char* argv[])
 	cout << "Test program !" << endl;
 	cout << "Number of arguments given to program : " << argc << endl;
 	
+	for(int i=0; i < argc; i++)
+	{
+		cout << "Argument " << i << ": " << argv[i] << endl;
+	}
 
 	return 0;
 }
