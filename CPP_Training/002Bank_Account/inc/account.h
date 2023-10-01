5efee9b4495957b9122bdc5400d6fa6e26350fbc
Waleed <waleed.alhammami@spaceconcordia.ca>
2023-10-01 11:00:08 -0400

    Some progress

--- CPP_Training/002Bank_Account/inc/account.h
+++ CPP_Training/002Bank_Account/inc/account.h
@@ -1,11 +1,16 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
+#include <string>
+
+using namespace std;
+
 class Account
 {
 private:
 	string name;
 	float balance;
 public:
-}
+	Account(string, float);
+};
 #endif
--- CPP_Training/002Bank_Account/src/account.cpp
+++ CPP_Training/002Bank_Account/src/account.cpp
@@ -1,1 +1,8 @@
 #include "account.h"
+
+Account::Account(string newName, float value)
+{
+	this->name = newName;
+	this->balance = value;
+	
+}
