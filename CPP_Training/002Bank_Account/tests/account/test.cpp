ab40d237276504f293964fe25136321a90fd121f
Waleed <waleed.alhammami@spaceconcordia.ca>
2023-10-03 20:52:00 -0400

    Finished setting up the unit testing environment

--- CPP_Training/002Bank_Account/tests/account/test.cpp
+++ CPP_Training/002Bank_Account/tests/account/test.cpp
@@ -13,4 +13,16 @@ TEST_GROUP(account_test_group)
 	{
 		
 	}
+};
+
+TEST(account_test_group, simple_test)
+{
+	int x = 5;
+	CHECK_EQUAL(x, 5);
+}
+
+TEST(account_test_group, simple_test_2)
+{
+	int x = 4;
+	CHECK_EQUAL(x, 5);
 }
