fd6a56f27e2520708a0972bc4ce695e5f494590a
Waleed <waleed.alhammami@spaceconcordia.ca>
2023-10-09 11:43:02 -0400

    Added more unit tests to the account module

--- CPP_Training/002Bank_Account/tests/account/test.cpp
+++ CPP_Training/002Bank_Account/tests/account/test.cpp
@@ -34,3 +34,33 @@ TEST(account_test_group, bad_deposit)
 	CHECK_EQUAL(ret, Deposit_Error);
 	CHECK_EQUAL(Balance,2000);
 }
+
+TEST(account_test_group, good_withdraw)
+{
+	Account testAccount("Test Account",2000);
+	retCode ret = testAccount.Withdraw(1000);
+	float Balance = testAccount.getBalance();
+
+	CHECK_EQUAL(ret, No_Error);
+	CHECK_EQUAL(Balance,1000);
+}
+
+TEST(account_test_group, bad_withdraw_1)
+{
+	Account testAccount("Test Account",2000);
+	retCode ret = testAccount.Withdraw(3000);
+	float Balance = testAccount.getBalance();
+
+	CHECK_EQUAL(ret, Withdraw_Error);
+	CHECK_EQUAL(Balance,2000);
+}
+
+TEST(account_test_group, bad_withdraw_2)
+{
+	Account testAccount("Test Account",2000);
+	retCode ret = testAccount.Withdraw(-500);
+	float Balance = testAccount.getBalance();
+
+	CHECK_EQUAL(ret, Withdraw_Error);
+	CHECK_EQUAL(Balance,2000);
+}
